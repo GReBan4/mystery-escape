f62bf7895462170f881de929ba4306f886b8798f
GReBan4 <69110246+GReBan4@users.noreply.github.com>
2020-08-22 02:26:00 +0900

    Add files via upload
    プログラムコード集。※2020/8/22時

---
+++ script/QBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QBox : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(CompareTag("QBox"))
+        {
+            Debug.Log("HIT");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
