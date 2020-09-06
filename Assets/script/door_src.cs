43ef458a71afd0a05b2f16b6ccbe0faedb972825
Chami <m0119236b3@edu.teu.ac.jp>
2020-09-06 10:23:19 +0900

    修正の修正

--- Assets/script/door_src.cs
+++ Assets/script/door_src.cs
@@ -6,11 +6,18 @@ public class door_src : MonoBehaviour
 {
     public GameObject door;
     Rigidbody rb;
+    // サウンド設定
+    AudioSource audioSource;
+    public AudioClip doorOpen;
+    public AudioClip doorClosed;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = door.GetComponent<Rigidbody>();
+        // AudioComponent取得
+        audioSource = GetComponent<AudioSource>();
+        
     }
     public void Open()
     {
@@ -21,5 +28,15 @@ public class door_src : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // 衝突した際のサウンド設定
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Human")
+        {
+            audioSource.clip = doorClosed;
+            audioSource.Play();
+        }
     }
 }
